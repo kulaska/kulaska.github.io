f52267a94b0bc3e8a4706ff513f15cb473e7ea22
kulaska <kulawork23@yandex.ru>
2019-03-11 23:04:07 +0400

    init

---
+++ JavaScript_Drum_Kit/main.js
@@ -0,0 +1,24 @@
+var down = false;
+
+const keys = [65, 83, 68, 70, 71, 72, 74, 75, 76];
+const sources = ['boom', 'clap', 'hihat', 'kick', 'openhat', 'ride', 'snare', 'tink', 'tom'];
+       
+document.addEventListener('keydown', evt => {
+    if (down) return;
+    down = true;        
+    for (let i = 0; i < keys.length; i++) {
+        let str = `[data-key="${keys[i]}"]`;            
+        if (evt.which == keys[i]) {               
+            document.querySelector(str).classList.add('playing');            
+            let audio = new Audio('sounds/' + sources[i] + '.wav');
+            audio.play();            
+        };
+        setTimeout(() => {
+            document.querySelector(str).classList.remove('playing');
+            down = false;
+        }, 100)
+    }       
+});    
+
+
+
