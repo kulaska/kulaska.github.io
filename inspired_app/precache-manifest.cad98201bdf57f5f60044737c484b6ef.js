9485799c1761c3db5958dd2a7fd8edc14143decb
Andrei Kulagin <kulawork23@yandex.ru>
2019-07-13 14:38:03 +0400

    init

---
+++ inspired_app/precache-manifest.cad98201bdf57f5f60044737c484b6ef.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f619c113c213204709af340b15b373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "116ce23346f34057baae",
+    "url": "/static/css/main.dd5095a2.chunk.css"
+  },
+  {
+    "revision": "6aa1e5f704c58ecac3b0",
+    "url": "/static/js/2.2fc94d6a.chunk.js"
+  },
+  {
+    "revision": "116ce23346f34057baae",
+    "url": "/static/js/main.c8509d7c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a590c128a8fb9f161309d863cf570293",
+    "url": "/static/media/churchill_thumb.a590c128.jpg"
+  },
+  {
+    "revision": "eb142266f410003939bd75b303ddee2b",
+    "url": "/static/media/edison_thumb.eb142266.png"
+  },
+  {
+    "revision": "052688d8d58c2b8671db1d31086993c7",
+    "url": "/static/media/elon_thumb.052688d8.jpg"
+  },
+  {
+    "revision": "fb25e7992fda0b5b60a17335887a9c6e",
+    "url": "/static/media/ferguson_thumb.fb25e799.jpg"
+  },
+  {
+    "revision": "096b8de2bfa383bda0faa9aabc7188a1",
+    "url": "/static/media/gates_thumb.096b8de2.jpg"
+  },
+  {
+    "revision": "238ac244eaef7efff086e076fe86cc9e",
+    "url": "/static/media/lincoln_thumb.238ac244.jpg"
+  },
+  {
+    "revision": "e5faf5929ec0e4a62d74feb5d33ae62a",
+    "url": "/static/media/lombardi_thumb.e5faf592.jpg"
+  },
+  {
+    "revision": "22c68586a5f3f17e642cf6487f40e0d5",
+    "url": "/static/media/muhammad_thumb.22c68586.jpg"
+  },
+  {
+    "revision": "617b727205af15af97a1b25c71729bd8",
+    "url": "/static/media/ronaldo_thumb.617b7272.jpg"
+  }
+]);
